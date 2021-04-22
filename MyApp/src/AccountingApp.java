92e13099c54c7bd813419ef2e0928b5c56041e8f
kkr <kkr1211@naver.com>
2021-04-22 18:43:53 +0900

    java1 마무리 해야지..

---
+++ MyApp/src/AccountingApp.java
@@ -0,0 +1,5 @@
+public class AccountingApp {
+    public static void main(String[] args){
+
+    }
+}
---
+++ Programming/src/ClassApp.java
@@ -0,0 +1,9 @@
+//https://www.youtube.com/watch?v=5z5OBTWLeQY&list=PLuHgQVnccGMAIluRRVsC1e79ri-dwnBmR&index=36
+
+public class ClassApp {
+    public static void main(String[] args) {
+        System.out.println(Math.PI);
+        System.out.println(Math.floor(1.6));
+        System.out.println(Math.ceil(1.6));
+    }
+}
